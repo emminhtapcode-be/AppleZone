0dd01dd848473ebecfddb9d74cf1e4097d4c1de6
MinhDam2k6 <damnhatminh16102006@gmail.com>
2026-06-28 22:39:25 +0700

    api products

--- applezone-backend/src/routes/products.js
+++ applezone-backend/src/routes/products.js
@@ -1,9 +1,16 @@
 const express = require('express');
-const { getProducts, getProduct } = require('../controllers/productController');
+const {
+  getProducts,
+  getProduct,
+  getProductVariants,
+  getProductsByCategory,
+} = require('../controllers/productController');
 
 const router = express.Router();
 
-router.get('/',    getProducts);
+router.get('/', getProducts);
+router.get('/category/:catId', getProductsByCategory);
+router.get('/:id/variants', getProductVariants);
 router.get('/:id', getProduct);
 
 module.exports = router;
